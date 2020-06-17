5e3af656b8e0250fd53ae5f55442264e6e04fe52
Rohit <Rohit@192.168.1.8>
2020-06-17 08:07:41 +0530

    Arrays

---
+++ Arrays/src/Exp1.java
@@ -0,0 +1,19 @@
+
+public class Exp1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int []elements=new int[] {1,2,3,4,5};
+		int len=elements.length;
+		int sum=0;
+        for(int i = 0; i < len ; i++)
+        {
+            
+            sum = sum + elements[i];
+        }
+        System.out.println("Sum:"+sum);
+        float average = (float)sum / len;
+        System.out.println("Average:"+average);
+	}
+
+}
---
+++ Arrays/src/Exp7.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class Exp7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = {12, 34, 12, 45, 67, 89};
+		List<Integer> distinctArray = new ArrayList<>();
+		
+		for (int item : array) {
+			if (!distinctArray.contains(item))
+				distinctArray.add(item);
+		}
+		
+		System.out.println(Arrays.toString(distinctArray.toArray()));
+
+	}
+
+}
